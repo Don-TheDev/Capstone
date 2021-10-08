5b808e27c502c7d0a663144040d6543d01b58806
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2021-10-08 10:37:13 -0600

    Added custom weather check action

--- Bot/actions/actions.py
+++ Bot/actions/actions.py
@@ -25,3 +25,18 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
+
+
+class ActionCheckWeather(Action):
+    def name(self) -> Text:
+        return "action_check_weather"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("Hello World! from a custom action")
+        return []
