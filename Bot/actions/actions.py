043707dcd15ea205622747579595ae8466b46d0c
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2021-10-13 10:59:16 -0600

    inital commit

--- Bot/actions/actions.py
+++ Bot/actions/actions.py
@@ -7,34 +7,21 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List
-
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-
-
-class ActionHelloWorld(Action):
-
-    def name(self) -> Text:
-        return "action_hello_world"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
-
-        return []
-
-
-class ActionCheckWeather(Action):
-    def name(self) -> Text:
-        return "action_check_weather"
-
-    def run(self,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World! from a custom action")
-        return []
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
---
+++ scripts/my_nltk_script.py
@@ -0,0 +1,15 @@
+from nltk.corpus import twitter_samples, nps_chat
+
+# twitter_posts = twitter_samples.strings()
+
+# nps_posts = nps_chat.xml_posts()
+
+# for post in nps_posts:
+#     print(post.attrib['class'])
+
+
+nps_words = nps_chat.words()
+
+for word in nps_words:
+    print(word.attrib['class'])
+    
