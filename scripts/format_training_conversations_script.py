09137c175752a43edfb698ba2dd28245f6d7cfa6
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2021-10-14 08:46:54 -0600

    formatting training data

--- scripts/format_training_conversations_script.py
+++ scripts/format_training_conversations_script.py
@@ -1,10 +1,38 @@
+from numpy import iterable
 import pandas as pd
+import json
 
-data = pd.read_csv("got_scripts_breakdown.csv", sep=';')
+# data = pd.read_csv("got_scripts_breakdown.csv", sep=';')
 
-characterName = 'jon'
+# characterName = 'jon'
 
-filtered_data = data[data.Name == characterName]
+# filtered_data = data[data.Name == characterName]
 
 
-print(filtered_data)
+# print(filtered_data)
+
+with open("downloads/train.json") as f:
+    tweetqa = json.load(f)
+
+lines = ["""version: \"2.0\"
+
+stories:
+#tweetqa stories
+
+"""]
+
+for tweet in tweetqa:
+    story = tweet["qid"]
+    user = tweet["Question"]
+    bot = tweet["Answer"][0]
+    lines.append(f"""  - story: {story}
+    steps:
+      - user: '{user}'
+      - bot: '{bot}'\n\n""")
+
+f = open("bot/data/core/tweetqa.yml", "w", encoding="utf-8")
+f.writelines(lines)
+
+f.close()
+
+# print(lines)
