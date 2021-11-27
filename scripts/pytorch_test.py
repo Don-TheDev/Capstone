9cfe156ffe65dd9c51e7056b83f5a231e6f33a14
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2021-11-27 16:00:15 -0700

    Revert "extract and lemmatize action-object pairs"
    This reverts commit 8f7ce234ba880a01061cc2d59c5f7073dfcecbaf.

--- scripts/pytorch_test.py
+++ scripts/pytorch_test.py
@@ -1,3 +1,4 @@
 import torch
 x = torch.rand(5, 3)
-print("torch.rand(5, 3):\n", x)
+print(x)
+ 
--- scripts/spacy_test.py
+++ scripts/spacy_test.py
@@ -1,6 +1,3 @@
 import spacy
 
-spacy.prefer_gpu()
-nlp = spacy.load("en_core_web_sm")
-
-print(nlp.component_names)
+spacy.require_gpu()
