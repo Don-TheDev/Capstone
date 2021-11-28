0984ed2ccc7b84a64f4044ddf2e4f3e2ee61e0a8
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2021-11-27 17:15:12 -0700

    created django projects with two apps and migrated

---
+++ donsai/omnichat_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OmnichatApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'omnichat_api'
---
+++ donsai/omnichat_web/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OmnichatWebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'omnichat_web'
