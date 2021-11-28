f9255e83b5b614a3d1fd7dc34479b8612f5b9fb8
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2021-11-27 18:17:41 -0700

    formatting

--- donsai/donsai/settings.py
+++ donsai/donsai/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c0d367c#2v*@s(d2zm*@7=)spa6palp)@+b%s85m##n))ow305'
+SECRET_KEY = \
+    'django-insecure-c0d367c#2v*@s(d2zm*@7=)spa6palp)@+b%s85m##n))ow305'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,16 +87,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+        + '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+        + '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+        + '.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+        + '.NumericPasswordValidator',
     },
 ]
 
